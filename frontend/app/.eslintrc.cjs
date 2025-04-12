518bec30cbdae6494a6be8c3d4510cc7ec075760
Hocine bouarara <hocibouarara08@gmail.com>
2025-04-12 22:35:18 +0100

    delete the project for create it again

--- frontend/app/.eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ["react-app", "react-app/jest", "prettier"],
-  plugins: ["prettier"],
-  rules: {
-    "prettier/prettier": ["error"],
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-  },
-};
